580f51d033832d458ec756edb0d0b11b1e3b9096
Mona LisaHeyClos <thiscarlos@gmail.com>
2022-01-03 14:43:11 -0800

    paginated card footer added, card tables added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+import CardFooter from './CardFooter';
+import CardTables from './CardTables';
+
 export default function Home() {
+  const [providers, setProviders] = useState([]);
+    // make a call to the api
+    // use the data to render the page
+    // async function getData() {
+    //     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    //     const data = await res.json();
+    //     // console.log(data);
+    //     setProviders(res.data)
+    // }
+    // const names = getData();
+
   return (
-    <div className="text-2xl text-red-200">
-      hello world
+    <div className="h-screen w-screen flex flex-col">
+      <div className="text-2xl text-center font-helvetica text-gray-500 uppercase tracking-wider">
+        Welcome to the Health Providers Dashboard
+      </div>
+      <CardTables />
+      <CardFooter />
     </div>
   )
 }
